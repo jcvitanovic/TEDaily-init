ea00df921413d1cedef60a971a7f11fd569f3a7d
Jelena Cvitanovic <cvitanovic.jelena@gmail.com>
2015-02-02 15:47:45 -0500

    iframe content template

--- http_test.py
+++ http_test.py
@@ -3,23 +3,24 @@ import json
 
 # https://api.ted.com/v1/talks/1.json?api-key=hzjevu53hy7rggd7w7rnuegf
 
-
-def myprint(d):
-  for k, v in d.iteritems():
-    if isinstance(v, dict):
-      myprint(v)
-    else:
-      print "{0} : {1}".format(k, v)
-
-
-id = 1
+id = 3
 key = 'hzjevu53hy7rggd7w7rnuegf'
 param = {'api-key' : key}
 url = "https://api.ted.com/v1/talks/" + str(id) + ".json"
 r = requests.get(url,params = param)
 print(r.url)
 
-#print(r.json())
-
 with open('data.txt', 'w') as outfile:
     json.dump(r.json(), outfile)
+
+resp_dict = r.json()
+print("talk name: "+ resp_dict["talk"]["name"])
+print("TAGS: ")
+for tag in resp_dict["talk"]["tags"]:
+	print(tag["tag"])
+
+print("RATINGS")
+#print(resp_dict["talk"]["ratings"])
+for k in resp_dict["talk"]["ratings"]:
+	print k["rating"]
+
