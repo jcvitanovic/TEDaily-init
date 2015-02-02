d181292b34aa5121d1e0eef9c66836194a731290
Jelena Cvitanovic <cvitanovic.jelena@gmail.com>
2015-02-02 15:02:13 -0500

    json response write to file test

--- http_test.py
+++ http_test.py
@@ -3,10 +3,23 @@ import json
 
 # https://api.ted.com/v1/talks/1.json?api-key=hzjevu53hy7rggd7w7rnuegf
 
+
+def myprint(d):
+  for k, v in d.iteritems():
+    if isinstance(v, dict):
+      myprint(v)
+    else:
+      print "{0} : {1}".format(k, v)
+
+
 id = 1
 key = 'hzjevu53hy7rggd7w7rnuegf'
 param = {'api-key' : key}
 url = "https://api.ted.com/v1/talks/" + str(id) + ".json"
 r = requests.get(url,params = param)
 print(r.url)
+
 #print(r.json())
+
+with open('data.txt', 'w') as outfile:
+    json.dump(r.json(), outfile)
